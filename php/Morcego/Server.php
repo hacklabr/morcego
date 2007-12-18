acf3b641eafa8f3a23024eff3ad962da5b5ca3c3
Luis Henrique Fagundes <lhfagundes@hacklab.com.br>
2007-12-18 21:05:29 +0000

    [FIX] accents now work in php server that comes with Morcego
    git-svn-id: https://scm.arca.ime.usp.br/morcego/trunk@200 b160122e-fef4-0310-8144-9a7482f4e04c

--- php/Morcego/Server.php
+++ php/Morcego/Server.php
@@ -10,16 +10,63 @@ class Morcego_Server extends XML_RPC_Server {
     var $graph;
 
     // Using for string encoding in Macintoshs
-    var $_macCharTable = array("C1" => "E7",
-			       "C9" => "83",
-			       "CD" => "EA",
-			       "D3" => "EE",
-			       "DA" => "F2",
-			       "E1" => "87",
-			       "E9" => "8E",
-			       "ED" => "92",
-			       "F3" => "97",
-			       "FA" => "9C");
+    var $_macCharTable = array(
+			       "C4" => "80", // Ä
+			       "C5" => "81", // Å
+			       "C7" => "82", // Ç
+			       "C9" => "83", // É
+			       "D1" => "84", // Ñ
+			       "D6" => "85", // Ö
+			       "DC" => "86", // Ü
+			       "E1" => "87", // á
+			       "E0" => "88", // à
+			       "E2" => "89", // â
+			       "E4" => "8A", // ä
+			       "E3" => "8B", // ã
+			       "E5" => "8C", // å
+			       "E7" => "8D", // ç
+			       "E9" => "8E", // é
+			       "E8" => "8F", // è
+			       "EA" => "90", // ê
+			       "EB" => "91", // ë
+			       "ED" => "92", // í
+			       "EC" => "93", // ì
+			       "EE" => "94", // î
+			       "EF" => "95", // ï
+			       "F1" => "96", // ñ
+			       "F3" => "97", // ó
+			       "F2" => "98", // ò
+			       "F4" => "99", // ô
+			       "F6" => "9A", // ö
+			       "F5" => "9B", // õ
+			       "FA" => "9C", // ú
+			       "F9" => "9D", // ù
+			       "FB" => "9E", // û
+			       "FC" => "9F", // ü
+
+			       "C0" => "CB", // À
+			       "C3" => "CC", // Ã
+			       "D5" => "CD", // Õ
+
+			       "FF" => "D8", // ÿ
+
+			       "C2" => "E5", // Â
+			       "CA" => "E6", // Ê
+			       "C1" => "E7", // Á
+			       "CB" => "E8", // Ë
+			       "C8" => "E9", // È
+			       "CD" => "EA", // Í
+			       "CE" => "EB", // Î
+			       "CF" => "EC", // Ï
+			       "CC" => "ED", // Ì
+			       "D3" => "EE", // Ó
+			       "D4" => "EF", // Ô
+
+			       "D2" => "F1", // Ò
+			       "DA" => "F2", // Ú
+			       "DB" => "F3", // Û
+			       "D9" => "F4", // Ù
+			       );
 
     var $win = false;
     var $mac = false;
@@ -33,7 +80,7 @@ class Morcego_Server extends XML_RPC_Server {
 		     'getVersion' => array('function' => array($this, 'getVersion')));
 
 	$this->win = preg_match("/Windows/", $_SERVER['HTTP_USER_AGENT']);
-	$this->mac = preg_match("/Macintosh/", $_SERVER['HTTP_USER_AGENT']);
+	$this->mac = preg_match("/Mac OS/", $_SERVER['HTTP_USER_AGENT']);
 	
 	$this->XML_RPC_Server($map);
     }
@@ -79,7 +126,6 @@ class Morcego_Server extends XML_RPC_Server {
     }
 
     function _recodeString($str) {
-
 	if ($this->win) {
 	    return utf8_decode($str);
 	}
@@ -87,11 +133,18 @@ class Morcego_Server extends XML_RPC_Server {
 	if ($this->mac) {
 	    $str = utf8_decode($str);
 
-	    foreach ($this->_macCharTable as $from => $to) {
-		$str = preg_replace("/".chr(hexdec($from))."/", chr(hexdec($to)), $str);
+	    $new = "";
+	    for ($i = 0; $i < strlen($str); $i++) {
+		$c = substr($str, $i, 1);
+		$isoCode = strtoupper(dechex(ord($c)));
+		if (isset($this->_macCharTable[$isoCode])) {
+		    $new .= chr(hexdec($this->_macCharTable[$isoCode]));
+		} else {
+		    $new .= $c;
+		}
 	    }
 
-	    return $str;
+	    return $new;
 	}
 
 	return $str;
